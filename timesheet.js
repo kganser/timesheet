f468ff6c12d623fbb7c1834aaf11b16afd5e2b7b
Klaus Ganser <kganser@gmail.com>
2015-09-13 03:14:19 -0500

    Initial checkin

---
+++ timesheet.js
@@ -0,0 +1,218 @@
+(function() {
+  var model = function(data, insert) {
+    var keys = !Array.isArray(data) && Object.keys(data),
+        model, elem;
+    return model = {
+      get: function(key) {
+        if (key == null) return data;
+        return data[key];
+      },
+      remove: function(key) {
+        var index = keys ? keys.indexOf(key) : key;
+        if (index >= 0) {
+          if (keys) {
+            keys.splice(index, 1);
+            delete data[key];
+          } else {
+            data.splice(index, 1);
+          }
+          if (elem) elem.removeChild(elem.childNodes[index]);
+        }
+        return model;
+      },
+      insert: function(value, key) {
+        var index = key == null ? data.length : key;
+        if (keys) {
+          if ((index = keys.indexOf(key)) < 0) {
+            index = keys.length;
+            keys.push(key);
+          } else if (elem) {
+            elem.removeChild(elem.childNodes[index]);
+          }
+          data[key] = value;
+        } else {
+          data.splice(index, 0, value);
+        }
+        if (elem) elem.insertBefore(jsml(insert(value, key, index, model)), elem.childNodes[index]);
+        return model;
+      },
+      insertAll: function(values) {
+        if (keys) {
+          Object.keys(values).forEach(function(key) {
+            model.insert(values[key], key);
+          });
+        } else {
+          values.forEach(function(value) {
+            model.insert(value);
+          });
+        }
+        return model;
+      },
+      view: function(parent) {
+        var data_ = data;
+        elem = parent;
+        data = keys ? {} : [];
+        keys = keys && [];
+        model.insertAll(data_);
+      }
+    };
+  }
+  var db = objectDB.open('timesheet', {entries: {}}),
+      days = 'Sun Mon Tues Wed Thurs Fri Sat'.split(' '),
+      months = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+      today = new Date(),
+      now = today.getTime(),
+      dayms = 24*60*60*1000,
+      issues = {}, dates = {},
+      issue, hours, entries, add, form, suggest, report;
+  var dateString = function(d) {
+    var y = d.getFullYear(),
+        m = d.getMonth()+1,
+        d = d.getDate();
+    return [y, m < 10 ? '0'+m : m, d < 10 ? '0'+d : d].join('-');
+  };
+  var dateIcon = function(date, click) {
+    return {div: {className: 'date '+days[date.getDay()].toLowerCase(), onclick: click, children: [
+      {div: days[date.getDay()]},
+      months[date.getMonth()]+' '+date.getDate()
+    ]}};
+  };
+  db.get('entries', false, function(path) {
+    if (path.length) return function(issue) { issues[issue] = 1; };
+  });
+  jsml([
+    {header: [
+      {nav: [
+        {span: {children: 'Record', onclick: function() { document.body.className = ''; }}},
+        {span: {children: 'Report', onclick: function() { document.body.className = 'show-report'; }}}
+      ]},
+      {div: {className: 'record', children: function(e) {
+        form = e;
+        return [
+          dateIcon(today),
+          {div: {className: 'issue', children: [
+            {input: {type: 'text', placeholder: 'issue', onkeyup: function(e) {
+              issue = this.value;
+              var items = issue.length < 2 ? [] : Object.keys(issues).filter(function(item) {
+                return ~item.toLowerCase().indexOf(issue.toLowerCase());
+              });
+              jsml(items.map(function(item) {
+                return {li: {children: item, onclick: function() {
+                  issue = item;
+                  e.target.value = item;
+                  jsml(null, suggest, true);
+                  hours.focus();
+                }}};
+              }), suggest, true);
+            }, onfocus: function() {
+              suggest.style.display = 'block';
+            }, onblur: function() {
+              setTimeout(function() {
+                suggest.style.display = 'none'; // TODO: more elegant solution?
+              }, 500);
+            }}},
+            {ul: function(e) { suggest = e; }}
+          ]}},
+          {input: {type: 'text', className: 'hours', placeholder: 'hours', children: function(e) { hours = e; }}},
+          {button: {children: function(e) { add = e; return 'Add'; }, disabled: true, onclick: function(e) {
+            this.disabled = true;
+            var date = dateString(today),
+                time = parseFloat(hours.value);
+            if (!time || time < 0) {
+              alert('Invalid number of hours');
+            } else {
+              var success = function() {
+                e.target.disabled = false;
+                entries.get(date).insert(time, issue);
+              };
+              db.put('entries/'+date+'/'+encodeURIComponent(issue), time).then(function(e) {
+                if (!e) return success();
+                var record = {}; record[issue] = time;
+                db.put('entries/'+date, record).then(success);
+              });
+            }
+          }}}
+        ];
+      }}},
+      {div: {className: 'report', children: [
+        {ul: new Array(36 + today.getDay()).join().split(',').map(function(x, i, days) {
+          var date = new Date(now-(days.length-i-1)*dayms),
+              value = dateString(date);
+          return {li: [dateIcon(date, function() {
+            this.classList.toggle('selected');
+            if (this.classList.contains('selected')) dates[value] = 1;
+            else delete dates[value];
+            db.get('entries', false, function(path) {
+              if (path.length) return path[0] in dates;
+            }).then(function(data) {
+              var totals = {}, total = 0;
+              Object.keys(data).forEach(function(date) {
+                Object.keys(date = data[date]).forEach(function(issue) {
+                  totals[issue] = (totals[issue] || 0) + date[issue];
+                });
+              });
+              jsml({ul: Object.keys(totals).concat([1]).map(function(issue, i, arr) {
+                var last = i == arr.length-1,
+                    time = last ? total : totals[issue];
+                if (!last) total += time;
+                return {li: {className: last ? 'total' : '', children: [
+                  {div: {className: 'time', children: time+' h'}},
+                  {div: {className: 'name', children: last ? 'Total' : issue}}
+                ]}};
+              })}, report, true);
+            });
+          })]};
+        })}
+      ]}}
+    ]},
+    {div: {className: 'content', children: [
+      {div: {className: 'record', children: [
+        {ul: (entries = model({}, function(value, date) {
+          date = date.split('-');
+          date = new Date(date[0], parseInt(date[1], 10)-1, parseInt(date[2], 10));
+          return {li: [
+            dateIcon(date, function() {
+              form.replaceChild(jsml(dateIcon(today = date)), form.firstChild);
+            }),
+            {ul: value.view}
+          ]};
+        })).view},
+        {button: {className: 'previous', children: function(e) {
+          var page = 0, days = 7;
+          (e.onclick = function() {
+            e.disabled = true;
+            var end = new Date(now-page*days*dayms);
+            db.get('entries', false, function(path) {
+              if (!path.length) return {
+                lowerBound: dateString(new Date(end.getTime() - (days-1)*dayms)),
+                upperBound: dateString(end),
+                descending: true
+              };
+            }).then(function(data) {
+              e.textContent = 'Previous';
+              e.disabled = add.disabled = false;
+              new Array(days).join().split(',').forEach(function(x, i) {
+                var date = dateString(new Date(now-(page*days+i)*dayms));
+                entries.insert(model(data[date] || {}, function(hours, issue, index, items) {
+                  return {li: [
+                    {button: {className: 'remove', children: '✕', onclick: function(e) {
+                      this.disabled = true;
+                      db.delete('entries/'+date+'/'+encodeURIComponent(issue)).then(function() {
+                        items.remove(issue);
+                      });
+                    }}},
+                    {div: {className: 'time', children: hours+' h'}},
+                    {div: {className: 'name', children: issue}}
+                  ]};
+                }), date);
+              });
+              page++;
+            });
+          })();
+          return 'Loading...';
+        }}}
+      ]}},
+      {div: {className: 'report', children: function(e) { report = e; }}}
+    ]}}
+  ], document.body);
+)());
