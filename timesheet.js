47bbb8eba860891719ec1e11c0a1c81a1f159446
Klaus Ganser <kganser@gmail.com>
2015-10-02 03:09:24 -0500

    Add GA tracking

--- timesheet.js
+++ timesheet.js
@@ -253,4 +253,11 @@
       {div: {className: 'report', children: function(e) { report = e; }}}
     ]}}
   ]}}, document.body);
+  
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-27434499-1', 'auto');
+  ga('send', 'pageview');
 }());
